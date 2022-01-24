69966fe70d27478004ce8b1b93838f0ee5186bf2
fabio-felici-sia <Fabio.Felici@sia.eu>
2022-01-24 18:31:30 +0000

    fix typo in storage variables

--- app_service/main.tf
+++ app_service/main.tf
@@ -61,12 +61,11 @@ resource "azurerm_app_service" "this" {
   }
 
   storage_account {
-    count = var.enable_storage ? 1 : 0
     name = var.storage_file_name
     type = var.storage_type
     account_name = var.storage_account_name
     share_name = var.storage_account_shared_name
-    mounth_path = var.storage_mount
+    mount_path = var.storage_mount
   }
 
   identity {
